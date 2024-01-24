e30eb909f92f476052259df10ff96297705cdb38
Eduard Florin Boza <Eduard-Florin.Boza@endava.com>
2024-01-24 20:19:50 +0200

    added create tables script. have to review them

---
+++ microservices/order-service/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,45 @@
+drop table if exists order;
+-- drop table if exists order_item;
+
+create table order
+(
+    order_id serial NOT NULL,
+    PRIMARY KEY (order_id),
+
+    CONSTRAINT fk_delivery_address
+        FOREIGN KEY (delivery_address_id)
+            REFERENCES delivery_address (delivery_address_id),
+
+    CONSTRAINT fk_order_item
+        FOREIGN KEY (order_item_id)
+            REFERENCES order_item (order_item_id)
+);
+
+create table IF NOT EXISTS order_item
+(
+    order_item_id serial NOT NULL,
+    amount serial,
+    PRIMARY KEY( order_item_id),
+    product serial REFERENCES product(order_item_id),
+
+    CONSTRAINT fk_order
+);
+
+create table product
+(
+    product_id serial  NOT NULL,
+    price      decimal NOT NULL,
+    name       varchar(255),
+    PRIMARY KEY (product_id)
+);
+
+create table delivery_address
+(
+    delivery_address_id serial NOT NULL,
+    street              varchar(255),
+    city                varchar(255),
+    state               varchar(255),
+    country             varchar(255),
+    postal_code         varchar(255),
+    PRIMARY KEY (delivery_address_id)
+);

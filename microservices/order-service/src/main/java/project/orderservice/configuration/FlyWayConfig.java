e8494f47ac8b3988ad80a81accc2da59fce26052
Eduard Florin Boza <Eduard-Florin.Boza@endava.com>
2024-01-28 17:33:45 +0200

    flyway scripts are generated well in postgresql(pgAdmin) and also in tests using Testcontainers

---
+++ microservices/order-service/src/main/java/project/orderservice/configuration/FlyWayConfig.java
@@ -0,0 +1,26 @@
+// package project.orderservice.configuration;
+//
+// import lombok.RequiredArgsConstructor;
+// import org.flywaydb.core.Flyway;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.core.env.Environment;
+//
+// @Configuration
+// @RequiredArgsConstructor
+// public class FlyWayConfig {
+//
+//     private final Environment environment;
+//
+//
+//     @Bean(initMethod = "migrate")
+//     public Flyway flyway() {
+//         return new Flyway(Flyway.configure()
+//             .baselineOnMigrate(true)
+//             .dataSource(environment.getRequiredProperty("spring.datasource.url"),
+//                 environment.getRequiredProperty("spring.datasource.username"),
+//                 environment.getRequiredProperty("spring.datasource.password"))
+//         );
+//     }
+//
+// }

1ca038940efee4768e0a7db8e3950559c962201d
Eduard Florin Boza <Eduard-Florin.Boza@endava.com>
2024-02-01 21:50:30 +0200

    added comments with what I have to do

--- microservices/order-service/src/main/java/project/orderservice/model/DeliveryAddress.java
+++ microservices/order-service/src/main/java/project/orderservice/model/DeliveryAddress.java
@@ -35,7 +35,7 @@ public class DeliveryAddress {
     private String country;
     @Column(name = "postal_code")
     private String postal_code;
-    @OneToMany(mappedBy = "deliveryAddress", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "deliveryAddress", fetch = FetchType.LAZY, orphanRemoval = true)
     private List<Order> orderList = new ArrayList<>();
 
 
@@ -51,3 +51,10 @@ public class DeliveryAddress {
     }
 
 }
+// de facut alt branch pe care fac modificarile si dupa ce e ceva gata fac PR si ii dau lui Daniel
+// de pus @Column la tot
+// de folosit formatter
+// de vazut care i treaba cu GenerationType.SEQUENCE si sa vad daca inserez in scripturile de flyway. Faza e sa am un sequence. sa vad daca se formeaza automat cu GenerationType.IDENTITY
+// de facut relatie ONE Product   TO   MANY OrderItems
+// de facut teste pe repository. TDD. mockito, junit5, hamcast?  faker?
+

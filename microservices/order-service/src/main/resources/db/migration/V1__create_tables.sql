7cdc85cb05c5833bfcb8b0be3dc3cb05830e27e2
Eduard Florin Boza <Eduard-Florin.Boza@endava.com>
2024-02-01 13:01:36 +0200

    updated scripts in order to create the corresponding db. Made script for order_item table to have as PK the product table's PK

--- microservices/order-service/src/main/resources/db/migration/V1__create_tables.sql
+++ microservices/order-service/src/main/resources/db/migration/V1__create_tables.sql
@@ -1,46 +1,45 @@
 CREATE TABLE delivery_address
 (
     delivery_address_id serial NOT NULL,
-    street varchar(255),
-    city varchar(255),
-    state varchar(255),
-    country varchar(255),
-    postal_code varchar(255),
+    street              varchar(255),
+    city                varchar(255),
+    state               varchar(255),
+    country             varchar(255),
+    postal_code         varchar(255),
     PRIMARY KEY (delivery_address_id)
 );
 
 CREATE TABLE product
 (
-    product_id serial NOT NULL,
-    price decimal NOT NULL,
-    name varchar(255),
+    product_id serial  NOT NULL,
+    price      decimal NOT NULL,
+    name       varchar(255),
     PRIMARY KEY (product_id)
-);
-
-CREATE TABLE order_item
-(
-    order_item_id serial NOT NULL,
-    product_id INT,
-    amount decimal,
-    PRIMARY KEY (order_item_id),
-
-    CONSTRAINT fk_product
-        FOREIGN KEY (product_id)
-            REFERENCES product (product_id)
 );
 
 CREATE TABLE orders
 (
-    order_id serial NOT NULL,
+    order_id            serial NOT NULL,
     delivery_address_id INT,
-    order_item_id INT,
     PRIMARY KEY (order_id),
 
     CONSTRAINT fk_delivery_address
         FOREIGN KEY (delivery_address_id)
-            REFERENCES delivery_address (delivery_address_id),
+            REFERENCES delivery_address (delivery_address_id)
+);
 
-    CONSTRAINT fk_order_item
+CREATE TABLE order_item
+(
+    order_item_id INT NOT NULL,
+    order_id      INT,
+    amount        decimal,
+    PRIMARY KEY (order_item_id),
+
+    CONSTRAINT fk_product_id
         FOREIGN KEY (order_item_id)
-            REFERENCES order_item (order_item_id)
+            REFERENCES product (product_id),
+
+    CONSTRAINT fk_order_id
+        FOREIGN KEY (order_id)
+            REFERENCES orders (order_id)
 );

4e6cdd95f3d75eaf0e0d9e1f7b885af8c3d1a1a3
Eduard Florin Boza <Eduard-Florin.Boza@endava.com>
2024-03-19 13:56:49 +0200

    updated testcontainers configuration. still facing that problem with the test. Not receiving objects from db, but in other tests it works

--- microservices/order-service/src/test/java/project/orderservice/repository/DeliveryAddressRepositoryTest.java
+++ microservices/order-service/src/test/java/project/orderservice/repository/DeliveryAddressRepositoryTest.java
@@ -1,10 +1,12 @@
 package project.orderservice.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -15,23 +17,25 @@ import project.orderservice.model.Order;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DeliveryAddressRepositoryTest extends BaseRepositoryTest {
+
   @Test
   @DisplayName(
       """
-       The test is successful because the DeliveryAddress has the correct Order in its orderList
-       """)
+           The test is successful because the DeliveryAddress has the correct Order in its orderList
+           """)
   void should_DeliveryAddressHaveTheCorrectOrder_WhenEntitiesAreSaved() {
     // Create and save DeliveryAddress
     DeliveryAddress deliveryAddress =
         DeliveryAddress.builder().street("Armeana 1").orderList(new ArrayList<>()).build();
-    deliveryAddressRepository.save(deliveryAddress);
 
     // Create and save Order, associating it with DeliveryAddress
     Order order =
         Order.builder().deliveryAddress(deliveryAddress).orderItemList(new HashSet<>()).build();
-    orderRepository.save(order);
 
     deliveryAddress.addOrder(order);
+
+    deliveryAddressRepository.save(deliveryAddress);
+    orderRepository.save(order);
 
     DeliveryAddress savedDeliveryAddress =
         deliveryAddressRepository
@@ -52,6 +56,23 @@ public class DeliveryAddressRepositoryTest extends BaseRepositoryTest {
     deliveryAddressRepository.deleteById(deliveryAddressId);
 
     assertFalse(deliveryAddressRepository.existsById(deliveryAddressId));
+  }
+
+  @Test
+  void shouldGetAllDeliveryAddresses() {
+    List<DeliveryAddress> deliveryAddressList =
+        List.of(
+            DeliveryAddress.builder().street("Armeana 100").build(),
+            DeliveryAddress.builder().street("Armeana 200").build());
+    deliveryAddressRepository.saveAll(deliveryAddressList);
+
+    List<DeliveryAddress> addressesReceived = deliveryAddressRepository.findAll();
+    assertThat(addressesReceived).hasSize(2);
+
+    DeliveryAddress savedDeliveryAddress =
+        deliveryAddressRepository
+            .findById(1L)
+            .orElseThrow(() -> new RuntimeException("Delivery Address not found"));
   }
 
   //    @Test

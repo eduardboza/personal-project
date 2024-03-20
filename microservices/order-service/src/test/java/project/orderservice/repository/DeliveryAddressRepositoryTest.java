51e120c7fd2028619eff6456729a27be20c768da
Eduard Florin Boza <Eduard-Florin.Boza@endava.com>
2024-03-20 19:30:34 +0200

    successfully finished some tests. It was an intelliJ error. After PC restart it worked. Unfortunately, I was debugging for 2 days but I found more about entityManager flush() and clear()

--- microservices/order-service/src/test/java/project/orderservice/repository/DeliveryAddressRepositoryTest.java
+++ microservices/order-service/src/test/java/project/orderservice/repository/DeliveryAddressRepositoryTest.java
@@ -1,8 +1,7 @@
 package project.orderservice.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -14,40 +13,46 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import project.orderservice.model.DeliveryAddress;
 import project.orderservice.model.Order;
 
-@DataJpaTest
+@DataJpaTest()
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DeliveryAddressRepositoryTest extends BaseRepositoryTest {
 
   @Test
   @DisplayName(
       """
-           The test is successful because the DeliveryAddress has the correct Order in its orderList
-           """)
+        The test is successful because the DeliveryAddress has the correct Order in its orderList
+        """)
   void should_DeliveryAddressHaveTheCorrectOrder_WhenEntitiesAreSaved() {
-    // Create and save DeliveryAddress
     DeliveryAddress deliveryAddress =
-        DeliveryAddress.builder().street("Armeana 1").orderList(new ArrayList<>()).build();
+        DeliveryAddress.builder()
+            .street("Armeana 1")
+            .city("Iasi")
+            .state("Europe")
+            .country("Ro")
+            .postalCode("XYZ")
+            .orderList(new ArrayList<>())
+            .build();
 
-    // Create and save Order, associating it with DeliveryAddress
     Order order =
         Order.builder().deliveryAddress(deliveryAddress).orderItemList(new HashSet<>()).build();
-
     deliveryAddress.addOrder(order);
 
-    deliveryAddressRepository.save(deliveryAddress);
-    orderRepository.save(order);
+    Order savedOrder = orderRepository.save(order);
+    assertTrue(deliveryAddress.getOrderList().contains(savedOrder));
 
-    DeliveryAddress savedDeliveryAddress =
+    List<Order> orders = orderRepository.findAll();
+
+    DeliveryAddress findByIdDeliveryAddress =
         deliveryAddressRepository
             .findById(deliveryAddress.getId())
             .orElseThrow(() -> new RuntimeException("Delivery Address not found"));
-    assertTrue(savedDeliveryAddress.getOrderList().contains(order));
+    assertTrue(findByIdDeliveryAddress.getOrderList().contains(savedOrder));
   }
 
   @Test
   @DisplayName("""
-  The test is successful because DeliveryAddress is deleted
-  """)
+        The test is successful because DeliveryAddress is deleted
+        """)
   void should_deleteDeliveryAddress() {
     DeliveryAddress deliveryAddress = DeliveryAddress.builder().street("Armeana 1").build();
     deliveryAddress = deliveryAddressRepository.save(deliveryAddress);
@@ -65,7 +70,6 @@ public class DeliveryAddressRepositoryTest extends BaseRepositoryTest {
             DeliveryAddress.builder().street("Armeana 100").build(),
             DeliveryAddress.builder().street("Armeana 200").build());
     deliveryAddressRepository.saveAll(deliveryAddressList);
-
     List<DeliveryAddress> addressesReceived = deliveryAddressRepository.findAll();
     assertThat(addressesReceived).hasSize(2);
 
@@ -75,39 +79,35 @@ public class DeliveryAddressRepositoryTest extends BaseRepositoryTest {
             .orElseThrow(() -> new RuntimeException("Delivery Address not found"));
   }
 
-  //    @Test
-  //  @DisplayName("""
-  //  The test is successful because DeliveryAddress is deleted and the Order associated to it is
-  // not deleted
-  // """)
-  //    void
-  // should_deleteDeliveryAddress_andNotDeleteOrder_whenDeliveryAddressIsAssociatedWithOrder() {
-  ////      Product product = Product.builder().name("Adidas
-  // 13").price(BigDecimal.valueOf(300.3D)).build();
-  ////      productRepository.save(product);
-  ////      OrderItem orderItem =
-  ////              OrderItem.builder().product(product).amount(BigDecimal.valueOf(3)).build();
-  ////      orderItemRepository.save(orderItem);
-  //
-  //      DeliveryAddress deliveryAddress =
-  //              DeliveryAddress.builder().street("Armeana 1").orderList(new
-  // ArrayList<>()).build();
-  //    Order order =
-  //        Order.builder().deliveryAddress(deliveryAddress).orderItemList(new HashSet<>()).build();
-  //      deliveryAddress.addOrder(order);
-  //      orderRepository.save(order);
-  //
-  //      // Assert that the Order and DeliveryAddress are saved
-  //      assertTrue(orderRepository.existsById(order.getId()));
-  //      assertTrue(deliveryAddressRepository.existsById(deliveryAddress.getId()));
-  //
-  //      // Delete the DeliveryAddress
-  //      deliveryAddressRepository.deleteById(deliveryAddress.getId());
-  //
-  //      // Assert that the DeliveryAddress is deleted
-  //      assertFalse(deliveryAddressRepository.existsById(deliveryAddress.getId()));
-  //
-  //      // Assert that the Order is NOT deleted
-  //      assertTrue(orderRepository.existsById(order.getId()));
-  //    }
+  @Test
+  @DisplayName(
+      """
+    The test is successful because DeliveryAddress is deleted and the Order associated to it is deleted
+   because we have just 1 order associated to this delivery address and have orphanRemoval = true on orderList and Order is not associated with other entities
+   """)
+  void should_deleteDeliveryAddress_andDeleteOrder_whenDeliveryAddressIsAssociatedWithOrder() {
+    DeliveryAddress deliveryAddress =
+        DeliveryAddress.builder().street("Armeana 1").orderList(new ArrayList<>()).build();
+    Order order =
+        Order.builder().deliveryAddress(deliveryAddress).orderItemList(new HashSet<>()).build();
+    deliveryAddress.addOrder(order);
+    orderRepository.save(order);
+
+    // Assert that the Order and DeliveryAddress are saved
+    assertTrue(orderRepository.existsById(order.getId()));
+    assertTrue(deliveryAddressRepository.existsById(deliveryAddress.getId()));
+
+    // Delete the DeliveryAddress
+    deliveryAddressRepository.deleteById(deliveryAddress.getId());
+
+    // Assert that the DeliveryAddress is deleted
+    assertFalse(deliveryAddressRepository.existsById(deliveryAddress.getId()));
+
+    // Assert that the Order is deleted because we have just 1 order associated to this delivery
+    // address and have orphanRemoval = true on orderList
+    // The orphanRemoval = true attribute ensures that if an Order entity is removed from the
+    // orderList, it will also be deleted from the database if it's not associated with any other
+    // DeliveryAddress.
+    assertFalse(orderRepository.existsById(order.getId()));
+  }
 }
